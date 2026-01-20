38176c18be56628de8d38c2b5dbb5cb0106cb84a
“Venu” <Venuchalla1993@gmail.com>
2026-01-20 14:53:35 -0600

    updated with jwt

---
+++ src/main/java/com/example/resilience/dto/UserJwtResponse.java
@@ -0,0 +1,16 @@
+package com.example.resilience.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserJwtResponse {
+    String username;
+    String jwtToken;
+}
